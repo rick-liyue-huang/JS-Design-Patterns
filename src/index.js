c1399c2fae8e50acf90a88013f7932b8ab7102fd
rick-liyue-huang <rick.liyue.huang@gmail.com>
2019-01-16 17:36:05 +1100

    some samples

---
+++ src/index.js
@@ -0,0 +1,5 @@
+
+// give some samples for OO and Design Patterns
+
+// 1. when call taxi, we can call type1 or type2, any taxi has plate and name; different type has different price, type1 is $1, type2 is $2; when taxi start drive it shows taxi infomation, and when finish drive it shows price under 5km. needs UML and ES6 code.
+
