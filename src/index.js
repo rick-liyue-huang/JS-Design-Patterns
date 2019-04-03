c53d51de9ce26fc824f2bf0717d2d6ad6eb4f204
rick-liyue-huang <rick.liyue.huang@gmail.com>
2019-04-03 17:40:41 +1100

    modify

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
 
-/*
-	Factory Pattern: encapsulate 'new',
- */
+class jQuery {
+	constructor(selector) {
+		let slice = Array.prototype.slice;
+		let dom = slice.call(document.querySelectorAll(selector));
+		let len = dom ? dom.length : 0;
+		for (let i = 0; i < len; i++) {
+			this[i] = dom[i];
+		}
+		this.length = len;
+		this.selector = selector || '';
+	}
+
+	append(node) {
+
+	}
+
+	addClass(name) {
+
+	}
+
+	html() {
+
+	}
+}
+
+window.$ = function(selector) {
+	// factory mode
+	return new jQuery(selector);
+}
+
+let $p = $('p');
+console.log($p);
+console.log($p.addClass)
+
+
 
